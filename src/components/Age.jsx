65836b91ec7b29b23fcc91dff05d3e51f8922a63
krishna <krishnasaiprakhya@gmail.com>
2023-11-27 20:32:19 +0530

    improved slider

--- src/components/Age.jsx
+++ src/components/Age.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { Slider } from "@mui/material";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 
 function Age({ selectedOptions, updateSelectedOptions }) {
   const navigate = useNavigate();
@@ -41,30 +43,69 @@ function Age({ selectedOptions, updateSelectedOptions }) {
         justifyContent: "center",
         alignItems: "center",
         height: "100vh",
-        fontSize: "30px",
+        backgroundColor: "#f0f0f0",
       }}
     >
-      <div style={{ textAlign: "center" }}>
-        <Slider
-          value={sliderValue}
-          onChange={handleSliderChange}
-          style={{ width: 900 }}
-          valueLabelDisplay="auto"
-          color="secondary"
-          defaultValue={20}
-          max={120}
-          marks={mark}
-        />
-        <p>Your Age is: {sliderValue}</p>
-        <Button
-          className="mt-3"
-          onClick={FormPage}
-          variant="contained"
-          color="success"
+      <Card
+        style={{
+          minWidth: "350px",
+          maxWidth: "800px",
+          display: "flex",
+          alignItems: "center",
+          height: "70%",
+        }}
+      >
+        <CardContent
+          style={{ textAlign: "center", padding: "40px", width: "100%" }}
         >
-          NEXT
-        </Button>
-      </div>
+          <h1
+            style={{
+              marginBottom: "30px",
+              fontFamily: "Arial, sans-serif",
+              fontSize: "28px",
+            }}
+          >
+            What's Your Age?
+          </h1>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column",
+            }}
+          >
+            <Slider
+              value={sliderValue}
+              onChange={handleSliderChange}
+              style={{ width: "700px", margin: "0 auto" }}
+              valueLabelDisplay="auto"
+              color="secondary"
+              defaultValue={20}
+              max={120}
+              marks={mark}
+            />
+            <p
+              style={{
+                marginTop: "20px",
+                fontWeight: "bold",
+                fontSize: "24px",
+              }}
+            >
+              Your Age is: {sliderValue}
+            </p>
+            <Button
+              className="mt-3"
+              onClick={FormPage}
+              variant="contained"
+              color="primary"
+              style={{ marginTop: "30px" }}
+            >
+              NEXT
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 }
