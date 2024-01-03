9b645404fce54ed4f42d3b94f1c4f52ebea1bd91
krishna <krishnasaiprakhya@gmail.com>
2024-01-03 14:07:49 +0530

    table_updated

---
+++ src/components/Modal.jsx
@@ -0,0 +1,68 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { Navigate, useNavigate } from "react-router-dom";
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 800,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function BasicModal({ stateObj }) {
+  const navigate = useNavigate();
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const handleClick = () => {
+    navigate("/userInfo", { state: stateObj });
+  };
+  return (
+    <div>
+      <Button onClick={handleOpen}>View Info</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Amogh
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            In China as well as France, the first people to perform dentistry
+            were barbers. They have been categorized into 2 distinct groups:
+            guild of barbers and lay barbers. The first group, the Guild of
+            Barbers, was created to distinguish more educated and qualified
+            dental surgeons from lay barbers. Guild barbers were trained to do
+            complex surgeries. The second group, the lay barbers, were qualified
+            to perform regular hygienic services such as shaving and tooth
+            extraction as well as basic surgery. However, in 1400, France made
+            decrees prohibiting lay barbers from practicing all types of
+            surgery. In Germany as well as France from 1530 to 1575 publications
+            completely devoted to dentistry were being published. Ambroise Paré,
+            often known as the Father of Surgery, published his own work about
+            the proper maintenance and treatment of teeth. Ambroise Paré was a
+            French barber surgeon who performed de
+          </Typography>
+          <Button
+            onClick={handleClick}
+            className="mt-5"
+            variant="contained"
+            color="success"
+          >
+            BOOK APPOINTMENT
+          </Button>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
---
+++ src/components/slotPage.jsx
@@ -0,0 +1,94 @@
+import Stars from "./Stars";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import React from "react";
+import Navbar from "./Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+import {
+  Button,
+  Table,
+  TableHead,
+  TableCell,
+  TableRow,
+  TableBody,
+  Box,
+} from "@mui/material";
+import Modal from "./Modal";
+function SlotPage() {
+  const location = useLocation();
+  const [updatedList, setUpdatedList] = useState(location.state);
+
+  let combinedArray = [];
+  console.log("hello");
+  for (let i = 0; i < location.state.doct_list.length; i++) {
+    combinedArray.push({
+      name: location.state.doct_list[i],
+      speciality: location.state.speciality,
+      hospital: location.state.hospitals_list[i],
+      rating: 4.8,
+      review: "Excellent",
+    });
+  }
+  const slotPageStyle = {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh",
+  };
+
+  const mainContentStyle = {
+    flex: 1,
+    paddingBottom: "60px", // Adjust the padding to accommodate the footer's height
+  };
+
+  const footerStyle = {
+    flexShrink: 0, // Prevents the footer from being resized
+  };
+  return (
+    <>
+      <Outlet />
+      <div style={slotPageStyle}>
+        <Navbar />
+        <div style={mainContentStyle} className="m-5">
+          <Table sx={{ minWidth: 650 }}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Doctor Name</TableCell>
+                <TableCell align="left">Speciality</TableCell>
+                <TableCell align="left">Hospital</TableCell>
+                <TableCell align="center">Ratings</TableCell>
+                <TableCell align="right">Book Appointment</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {combinedArray.map((doctor, index) => (
+                <TableRow key={index}>
+                  <TableCell>{doctor.name}</TableCell>
+                  <TableCell align="left">{doctor.speciality}</TableCell>
+                  <TableCell align="left">{doctor.hospital}</TableCell>
+                  <TableCell align="center">
+                    <Box
+                      sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Stars stars={doctor.rating} review={doctor.review} />
+                    </Box>
+                  </TableCell>
+                  <TableCell align="right">
+                    <Modal stateObj={updatedList} />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+        <Footer stle={footerStyle} />
+      </div>
+    </>
+  );
+}
+
+export default SlotPage;
