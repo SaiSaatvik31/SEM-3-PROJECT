3358615c341c92209f255bf109f2e33da55fd1b6
Amogh-1416 <samudralaamogh16@gmail.com>
2024-01-25 10:04:15 +0530

    home page updated

---
+++ server/models/doctor_avail_new.js
@@ -0,0 +1,14 @@
+const mongoose=require('mongoose');
+const doctor=new mongoose.Schema({
+  doc_name : String,
+  doc_id:String,
+  speciality:String,
+  doctor_desc:String,
+  hospital_name:String,
+  availability:Object,
+  amt:String,
+  status:String
+
+},{collection:'doc_avail_new'})
+const model=mongoose.model('doc_avail_new',doctor)
+module.exports=model
